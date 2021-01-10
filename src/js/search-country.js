4880774fac25cfc6e39273048adc7c638c9bb8ee
Ivanov Evhenii <ivanov191192@gmail.com>
2021-01-10 03:02:00 +0200

    fix

--- src/js/search-country.js
+++ src/js/search-country.js
@@ -2,6 +2,7 @@ import lodashDebounce from 'lodash.debounce';
 import country from '../template/country.hbs';
 import countryList from '../template/country-list.hbs';
 import notifyFunctions from '../js/notification';
+import countriesAPI from '../js/fetchCountries';
 const refs = {
   inputRef: document.querySelector('input'),
   listRef: document.querySelector('.country'),
@@ -11,32 +12,26 @@ let nameCountry;
 const debounceFetchCountry = lodashDebounce(e => {
   nameCountry = e.target.value;
   refs.listRef.innerHTML = '';
-  fetchCountry(nameCountry);
+  countriesAPI.fetchCountries(nameCountry).then(data => listOfCountries(data));
 }, 500);
 
 refs.inputRef.addEventListener('input', debounceFetchCountry);
 
-function fetchCountry(nameCountry) {
-  fetch(`https://restcountries.eu/rest/v2/name/${nameCountry}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      if (data.length > 10) {
-        notifyFunctions.notifyError();
-        return;
-      }
-      if (data.length >= 2 && data.length <= 10) {
-        notifyFunctions.notifyNotice();
-        const markup = countryList(data);
-        refs.listRef.insertAdjacentHTML('beforeend', markup);
-        return;
-      }
-      if (data.length === 1) {
-        notifyFunctions.notifySuccess();
-        const markup = country(data);
-        refs.listRef.insertAdjacentHTML('beforeend', markup);
-        return;
-      }
-    });
+function listOfCountries(data) {
+  if (data.length > 10) {
+    notifyFunctions.notifyError();
+    return;
+  }
+  if (data.length >= 2 && data.length <= 10) {
+    notifyFunctions.notifyNotice();
+    const markup = countryList(data);
+    refs.listRef.insertAdjacentHTML('beforeend', markup);
+    return;
+  }
+  if (data.length === 1) {
+    notifyFunctions.notifySuccess();
+    const markup = country(data);
+    refs.listRef.insertAdjacentHTML('beforeend', markup);
+    return;
+  }
 }
